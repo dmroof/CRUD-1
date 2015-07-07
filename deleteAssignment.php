9965e9815055de105a898f7d0a679b11eb59c8c9
dmroof <dmroof@svsu.edu>
2015-07-06 21:06:42 -0400

    Create deleteAssignment.php

---
+++ deleteAssignment.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * filename	: deleteAssignment.php
+ * author	: David Roof 
+ * course	: CIS 355
+ * purpose	: This program prompts the user if they want to delete the record.
+ *			  If the user clicks "Yes" then a query is submitted to delete the record.
+ *			  Else the user is redirected back to the assignments.php
+ *
+ * design	: 
+ *			
+ *			1. Display user whether or not to delete 
+ *			2. Submit Query to delete
+ *			
+*/
+    require 'database.php';
+    $id = 0;
+     
+    if ( !empty($_GET['id'])) {
+        $id = $_REQUEST['id'];
+    }
+     
+    if ( !empty($_POST)) {
+        // keep track post values
+        $id = $_POST['id'];
+         
+        // delete data
+        $pdo = Database::connect();
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "DELETE FROM assignments  WHERE id = ?";
+        $q = $pdo->prepare($sql);
+        $q->execute(array($id));
+        Database::disconnect();
+        header("Location: assignments.php");
+         
+    }
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+  <link   href="https://maxcdn.bootstrapcdn.com/bootstrap/2.3.2/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/2.3.2/js/bootstrap.min.js"></script>
+</head>
+ 
+<body>
+    <div class="container">
+     
+                <div class="span10 offset1">
+                    <div class="row">
+                        <h3>Delete an Assignment</h3>
+                    </div>
+                     
+                    <form class="form-horizontal" action="deleteAssignment.php" method="post">
+                      <input type="hidden" name="id" value="<?php echo $id;?>"/>
+                      <p class="alert alert-error">Are you sure to delete ?</p>
+                      <div class="form-actions">
+                          <button type="submit" class="btn btn-danger">Yes</button>
+                          <a class="btn" href="assignments.php">No</a>
+                        </div>
+                    </form>
+                </div>
+                 
+    </div> <!-- /container -->
+  </body>
+</html>
